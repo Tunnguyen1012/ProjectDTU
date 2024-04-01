6399a5d36e9c9ce7e25394c4a3815d8c766c5272
PC <PC@DESKTOP-LAPTOP.lan>
2024-04-01 16:28:13 +0700

    First Commit

---
+++ src/Basic/BaiThucHanh.java
@@ -0,0 +1,21 @@
+package Basic;
+
+public class BaiThucHanh {
+	public static void main(String[] args) {
+        // Khai báo và khởi tạo hai số
+        double number1 = 10.5;
+        double number2 = 5.2;
+
+        // Thực hiện các phép toán cơ bản
+        double sum = number1 + number2;
+        double difference = number1 - number2;
+        double product = number1 * number2;
+        double quotient = number1 / number2;
+
+        // In kết quả
+        System.out.println("\nSum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
