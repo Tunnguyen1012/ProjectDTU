90954c150a780075ff8bfceeee200e4e4104fe05
Asus <Asus@172.25.1.239>
2024-04-02 08:58:40 +0700

    First Commit

--- src/Basic/BaiThucHanh.java
+++ src/Basic/BaiThucHanh.java
@@ -2,8 +2,8 @@ package Basic;
 public class BaiThucHanh {
 	public static void main(String[] args) {
         // Khai báo và khởi tạo hai số
-        double number1 = 10.5;
-        double number2 = 5.2;
+        double number1 = 25;
+        double number2 = 10;
 
         // Thực hiện các phép toán cơ bản
         double sum = number1 + number2;
